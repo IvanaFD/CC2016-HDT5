99edd733a6eba182895a0b6fadfe9ce3890be546
IvanaFD <ivanafigueroa0936@gmail.com>
2025-03-05 22:09:42 -0600

    encabezado

--- simulator.py
+++ simulator.py
@@ -1,3 +1,14 @@
+
+#  Universidad del Valle de Guatemala
+#  Algoritmos y Estructuras de Datos - Sección 31
+#  Hoja de Trabajo 5
+#  Integrantes:
+#  Diana Sosa 241040
+#  Biancka Raxón 24960
+#  Ivana Figueroa 24785
+ 
+
+
 # Referencias: 
 # - https://www.youtube.com/watch?v=BqZxmbhJ14Q
 
